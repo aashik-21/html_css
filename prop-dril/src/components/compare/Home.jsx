9112b4d7340cf89cd74bb640acccdd8a82225270
aashik-21 <aashikmi460@gmail.com>
2026-01-14 17:14:36 +0530

    asdsdsad

--- prop-dril/src/components/compare/Home.jsx
+++ prop-dril/src/components/compare/Home.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../../assets/foods.json";
 export const Home = () => {
+    const [products] = useState(data);
     return (
-        <div>
-            <p>This is Home pages</p>
+        <div className="product-container">
+            {products.map((product) => {
+                return (
+                    <div key={product.id}>
+                        <h3>{product.name}</h3>
+                    </div>
+                );
+            })}
         </div>
     );
 };
